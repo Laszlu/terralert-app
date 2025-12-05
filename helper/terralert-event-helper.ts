15117629e8ff50a2596cf8a490c1ed2a9905b144
Laszlu <maurice.ferreyra@gmail.com>
2025-12-05 12:01:17 +0100

    TODO comment

--- helper/terralert-event-helper.ts
+++ helper/terralert-event-helper.ts
@@ -160,6 +160,8 @@ export function getMarkerForHistoryEvent(event: TerralertEvent, colorIndex: numb
     return null;
 }
 
+
+// TODO: SOME EVENTS HAVE ERRORS IN THEIR TAIL!!!!!!!!!
 export function getPolylineForEventWithColor(
     event: TerralertEvent,
     colorIndex: number
